86766bbba83cf89aba7c5a066605692ac48a375a
Stelly <105381685+heliogoodbye@users.noreply.github.com>
2024-04-29 11:37:54 -0500

    Create tm-sponsor-carousel.js

---
+++ js/tm-sponsor-carousel.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+    $('.slick-carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
